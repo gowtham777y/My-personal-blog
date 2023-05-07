88fe1d63cf8f2ee809df18ff5279a2c00300ff85
Gowtham Yarlagadda <gowtham@Gowthams-MacBook-Air.local>
2023-05-07 22:38:44 +0530

    Create home, about and contact pages and get requests

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -15,11 +13,17 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/",function(req,res){
+  res.render("home",{startingContent: homeStartingContent});
+});
 
+app.get("/about",function(req,res){
+  res.render("about",{content: aboutContent});
+});
 
-
-
-
+app.get("/contact",function(req,res){
+  res.render("contact",{content: contactContent});
+});
 
 
 
